9476b00a1834b9f77b01370c3dee0ea303b2e5e7
av_korotkih <potylchakav@gmail.com>
2020-11-17 14:07:26 +0300

    no message

--- Create_Index_Like.sql
+++
@@ -1,6 +0,0 @@
-use snet0611;
-
-alter table like_post add index IX_Like_Post_UserID (user_id);
-alter table like_post add index IX_Like_Post_PostID (post_id);
-
-alter table like_photos add index IX_Like_Photos_UserID (user_id);
